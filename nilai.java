b66ae35989b58c6b308e5b3444b87809cd0985c3
JauzaDffa <dafafarela@gmail.com>
2026-01-07 22:49:09 +0700

    Update nilai.java

--- nilai.java
+++ nilai.java
@@ -1,10 +1,10 @@
-public class nilai implements Penilaian { // Memenuhi syarat 5.9
+public class nilai implements Penilaian { 
     private String npmMahasiswa;
     private String kodeMataKuliah;
     private double nilaiTugas;
     private double nilaiUTS;
     private double nilaiUAS;
-    public static int totalNilai = 0; // Syarat 5.2 (Static Variable)
+    public static int totalNilai = 0; // Static variable
 
     // Constructor Parameter (Syarat 5.4)
     public nilai(String npm, String kode, double tgs, double uts, double uas) {
@@ -13,13 +13,12 @@ public class nilai implements Penilaian { // Memenuhi syarat 5.9
         this.nilaiTugas = tgs;
         this.nilaiUTS = uts;
         this.nilaiUAS = uas;
-        totalNilai++; // Menambah hitungan setiap objek dibuat
+        totalNilai++; // 
     }
 
-    // --- Implementasi Interface (Syarat 5.9) ---
+    // Implementasi Interface 
     @Override
     public double hitungNilaiAkhir() {
-        // Rumus: Tugas 30%, UTS 30%, UAS 40%
         return (nilaiTugas * 0.3) + (nilaiUTS * 0.3) + (nilaiUAS * 0.4);
     }
 
@@ -37,6 +36,4 @@ public class nilai implements Penilaian { // Memenuhi syarat 5.9
     public void tampilInfo() {
         System.out.println("NPM: " + npmMahasiswa + " | MK: " + kodeMataKuliah + " | Akhir: " + hitungNilaiAkhir() + " | Grade: " + tentukanGrade());
     }
-    
-    // Jangan lupa Getter & Setter untuk Encapsulation (Syarat 5.5)
 }
