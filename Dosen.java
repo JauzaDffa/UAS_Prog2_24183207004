feb4631cfe6876df2e2b3031c1aaab9ee9413740
JauzaDffa <dafafarela@gmail.com>
2026-01-07 22:52:40 +0700

    Update Dosen.java

--- Dosen.java
+++ Dosen.java
@@ -18,6 +18,7 @@ public class Dosen extends User{
     }
     @Override
     public void tampilInfo(){
-        System.out.println("NIDN : " + getid() + ", Nama Dosen: " + getNama() + ", Mata Kuliah: " + matakuliah);
+        System.out.println("NIDN : " + getId() + ", Nama Dosen: " + getNama() + ", Mata Kuliah: " + matakuliah);
     }
+
 }
